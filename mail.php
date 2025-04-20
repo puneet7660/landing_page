1f585d968b810c73ee1375ca8c9c834d4935a986
puneet7660 <73326942+puneet7660@users.noreply.github.com>
2025-04-20 11:07:21 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Set the recipient email address
-    $recipient = "your-email@example.com"; // Change this to your email
+    $recipient = "puneet7660@gmail.com"; // Change this to your email
 
     // Set the email subject
     $subject = "New contact from $name";
